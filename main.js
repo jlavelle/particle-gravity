6bfd7e3965e6320d438b5b1e84bff5073ea0606c
John C. Lavelle <jlavelle604@gmail.com>
2018-03-06 17:39:50 -0500

    Refactor

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ let sim
 
 const last = (n, as) => as.slice(as.length - n)
 
-const angle = (p1, p2) => Math.atan2(p2.y - p1.y, p2.x - p1.x)
-const momentum = ({ velocity, mass }) => velocity.copy().mult(mass)
-const kineticEnergy = ({ velocity, mass }) => 0.5 * mass * (velocity.copy().mag() ^ 2)
+const angle = (p1, p2) => Math.atan2(p2.pos.y - p1.pos.y, p2.pos.x - p1.pos.x)
+const momentum = ({ vel, mass }) => vel.copy().mult(mass)
+const kineticEnergy = ({ vel, mass }) => 0.5 * mass * (vel.copy().mag() ^ 2)
 
 const forceVec = (f, p1, p2) => {
   const a = angle(p1, p2)
@@ -17,34 +17,21 @@ const forceVec = (f, p1, p2) => {
 }
 
 const gravity = (p1, p2) => {
-  const distance = dist(p1.x, p1.y, p2.x, p2.y)
+  const distance = dist(p1.pos.x, p1.pos.y, p2.pos.x, p2.pos.y)
   const f = (G * p1.mass * p2.mass) / (distance ^ 2)
   if (f === Infinity || distance === 0) return createVector(0, 0)
-  const fV = forceVec(f, p1, p2)
-  return fV
+  return forceVec(f, p1, p2)
 }
 
-const Particle = (position, velocity, mass, color, history=[], lastForce=null) => {
-  return {
-    get x() {
-      return position.x
-    },
-    get y() {
-      return position.y
-    },
-    position,
-    mass,
-    color,
-    velocity,
-    history,
-    lastForce,
-    update(force) {
-      const a = p5.Vector.div(force, mass)
-      const v = velocity.add(a)
-      const oldP = position.copy()
-      const p = position.add(v)
-      return Particle(p, v, mass, color, [...last(500, history), oldP], force)
-    }
+const mkParticle = (pos, vel, mass, color, hist = [], lastf = null) => ({ pos, vel, mass, color, hist, lastf })
+
+const Particle = {
+  update: p => force => {
+    const { mass, vel, pos, hist } = p
+    const a = p5.Vector.div(force, mass)
+    const v = vel.add(a)
+    const np = pos.add(v)
+    return { ...p, vel: v, pos: np, lastf: force, hist: [...last(500, hist), pos.copy()] }
   }
 }
 
@@ -54,38 +41,40 @@ const Simulation = (particles) => {
     step() {
       return Simulation(particles.map(p => {
         const force = particles.reduce((acc, other) => acc.add(gravity(p, other)), createVector(0, 0))
-        return p.update(force)
+        return Particle.update(p)(force)
       }))
     }
   }
 }
 
+const showVec = (p, v, scale, color) => {
+  const { x, y } = p
+  const { x: vx, y: vy } = v
+  stroke(...color)
+  line(x, y, x + vx * scale, y + vy * scale)
+  noStroke()
+}
+
 const render = ({ particles }) => {
   particles.forEach(p => {
-    fill(...p.color)
-    const r = 10 + (p.mass / 50)
-    ellipse(p.x, p.y, r, r)
+    const { color, mass, pos, vel, lastf, hist } = p
+    const { x, y } = pos
+    fill(...color)
+    const r = 10 + (mass / 50)
+    ellipse(x, y, r, r)
+    
     if (showMomentum) {
       const m = momentum(p)
       fill(0)
-      text(`${m.mag().toFixed(3)}`, p.x + 10, p.y + 10)
-      stroke(100)
-      line(p.x, p.y, p.x + m.x * 2, p.y + m.y * 2)
-      noStroke()
+      text(`${m.mag().toFixed(3)}`, x + 10, y + 10)
     }
-    if (showVelocity) {
-      stroke(100)
-      line(p.x, p.y, p.x + p.velocity.x * 2, p.y + p.velocity.y * 2)
-    }
-    if (showForce && p.lastForce) {
-      stroke(0)
-      line(p.x, p.y, p.x + p.lastForce.x * 10, p.y + p.lastForce.y * 10)
-      
-    }
+    if (showVelocity) showVec(pos, vel, 2, [100])
+    if (showForce && lastf) showVec(pos, lastf, 2, [100])
     noStroke()
-    p.history.forEach((pos, i) => {
-      const tp = p.color.slice()
-      tp[3] = (i / p.history.length) * 255
+    
+    hist.forEach((pos, i) => {
+      const tp = color.slice()
+      tp[3] = (i / hist.length) * 255
       fill(...tp)
       ellipse(pos.x, pos.y, r / 3, r / 3)
     })
@@ -106,11 +95,11 @@ function setup() {
   createCanvas(1000, 1000)
   noStroke()
   sim = Simulation([
-    Particle(createVector(500, 300), createVector(13, 0), 1, [0, 0, 255]),
-    Particle(createVector(500, 350), createVector(13, 0), 1, [255, 0, 0]),
-    Particle(createVector(500, 400), createVector(12, 0), 1, [0, 255, 0]),
-    Particle(createVector(500, 450), createVector(12, 0), 1, [100, 100, 100]),
-    Particle(createVector(500, 500), createVector(0, 0), 1500, [255, 255, 0])
+    mkParticle(createVector(500, 300), createVector(13, 0), 5, [0, 0, 255]),
+    mkParticle(createVector(500, 350), createVector(13, 0), 5, [255, 0, 0]),
+    mkParticle(createVector(500, 400), createVector(4, 0), 2, [0, 255, 0]),
+    mkParticle(createVector(500, 450), createVector(12, 0), 1, [100, 100, 100]),
+    mkParticle(createVector(500, 500), createVector(0, 0), 1500, [255, 255, 0])
   ])
 }
 
